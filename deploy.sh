410099550988c1bcd4f0c6b964ea8b26f74dd854
Sebastian Elisa Pfeifer <sebastian.pfeifer@unicorncloud.org>
2018-01-31 17:58:08 +0100

    privileges

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 find . -type f | xargs dos2unix > /dev/null 2>&1
-ansible-playbook deploy.yml --user=spfeifer --ask-become-pass
+ansible-playbook deploy.yml --user=deploy --ask-become-pass
