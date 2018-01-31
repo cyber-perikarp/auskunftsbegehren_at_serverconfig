1aad2a4702f2cf709427d9b2ca9ac9297982182d
Sebastian Elisa Pfeifer <sebastian.pfeifer@unicorncloud.org>
2018-01-31 21:59:21 +0100

    docker, fixes, auskunftsbegehren.at tls key

--- roles/01-system/templates/opt/update-ssl.sh
+++ roles/01-system/templates/opt/update-ssl.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
 certbot --standalone certonly --email sebastian.pfeifer@unicorncloud.org --agree-tos --must-staple --keep-until-expiring --non-interactive --rsa-key-size 4096 --expand -d goatberger.cyber-perikarp.eu
+certbot --standalone certonly --email sebastian.pfeifer@unicorncloud.org --agree-tos --must-staple --keep-until-expiring --non-interactive --rsa-key-size 4096 --expand -d auskunftsbegehren.at -d www.auskunftsbegehren.at
+
 
 exit 0
