f97b406fdee1b40e556365dc96817645ff0e4f1a
Sebastian Elisa Pfeifer <sebastian.pfeifer@unicorncloud.org>
2018-02-02 15:33:22 +0100

    tls config, when renewing cert also renew ocsp files

--- roles/01-system/templates/opt/update-ssl.sh
+++ roles/01-system/templates/opt/update-ssl.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 service nginx stop
+
 certbot --standalone certonly --email sebastian.pfeifer@unicorncloud.org --agree-tos --must-staple --keep-until-expiring --non-interactive --rsa-key-size 4096 --expand -d goatberger.cyber-perikarp.eu
 certbot --standalone certonly --email sebastian.pfeifer@unicorncloud.org --agree-tos --must-staple --keep-until-expiring --non-interactive --rsa-key-size 4096 --expand -d auskunftsbegehren.at -d www.auskunftsbegehren.at
+bash /opt/update-stapling.sh
 
 service nginx start
