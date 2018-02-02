cb3ab9f2353c13edda349d4ee7c005166a039056
Sebastian Elisa Pfeifer <sebastian.pfeifer@unicorncloud.org>
2018-02-02 15:03:45 +0100

    mailserver, webmail

--- roles/01-system/templates/opt/update-ssl.sh
+++ roles/01-system/templates/opt/update-ssl.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 service nginx stop
 certbot --standalone certonly --email sebastian.pfeifer@unicorncloud.org --agree-tos --must-staple --keep-until-expiring --non-interactive --rsa-key-size 4096 --expand -d goatberger.cyber-perikarp.eu
-certbot --standalone certonly --email sebastian.pfeifer@unicorncloud.org --agree-tos --must-staple --keep-until-expiring --non-interactive --rsa-key-size 4096 --expand -d mail.auskunftsbegehren.at -d auskunftsbegehren.at -d www.auskunftsbegehren.at
+certbot --standalone certonly --email sebastian.pfeifer@unicorncloud.org --agree-tos --must-staple --keep-until-expiring --non-interactive --rsa-key-size 4096 --expand -d auskunftsbegehren.at -d www.auskunftsbegehren.at
 
 service nginx start
