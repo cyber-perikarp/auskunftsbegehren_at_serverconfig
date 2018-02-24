f382bbb22598b83c504b1760697b9e62cabda295
Sebastian Elisa Pfeifer <sebastian.pfeifer@unicorncloud.org>
2018-02-24 12:57:06 +0100

    app

--- roles/01-system/templates/opt/update-ssl.sh
+++ roles/01-system/templates/opt/update-ssl.sh
@@ -2,7 +2,8 @@
 service nginx stop
 
 certbot --standalone certonly --email sebastian.pfeifer@unicorncloud.org --agree-tos --must-staple --keep-until-expiring --non-interactive --rsa-key-size 4096 --expand -d goatberger.cyber-perikarp.eu
-certbot --standalone certonly --email sebastian.pfeifer@unicorncloud.org --agree-tos --must-staple --keep-until-expiring --non-interactive --rsa-key-size 4096 --expand -d auskunftsbegehren.at -d www.auskunftsbegehren.at -d mail.auskunftsbegehren.at
+certbot --standalone certonly --email sebastian.pfeifer@unicorncloud.org --agree-tos --must-staple --keep-until-expiring --non-interactive --rsa-key-size 4096 --expand -d auskunftsbegehren.at -d www.auskunftsbegehren.at -d mail.auskunftsbegehren.at -d dev.auskunftsbegehren.at
+
 bash /opt/update-stapling.sh
 
 service nginx start
