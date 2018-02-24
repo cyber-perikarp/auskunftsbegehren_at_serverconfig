d9096676fa94c47c01b2aab33185519937e3dfb6
Sebastian Elisa Pfeifer <sebastian.pfeifer@unicorncloud.org>
2018-02-24 14:18:59 +0100

    app

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 find . -type f | xargs dos2unix > /dev/null 2>&1
 source ./passwords.sh
-ansible-playbook deploy.yml --user=deploy --ask-become-pass --extra-vars "mysql_app_pw=$mysql_app_pw"
+ansible-playbook deploy.yml --user=deploy --ask-become-pass --extra-vars "mysql_app_pw=$mysql_app_pw mail_app_pw=$mail_app_pw"
